7e5c7f4e1f5c8c1b473c140fac3c314c3cbdef7d
Gabriel Costa <gabriellcostta13@gmail.com>
2026-08-20 15:50:43 -0300

    Fix: Resolve todos os conflitos de testes

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,54 +1,43 @@
 /// <reference types="cypress"/>
 
-const { faker } = require('@faker-js/faker');
+const { faker } = require("@faker-js/faker");
 
-describe('Funcionalidade: Cadastro no Hub de Leitura', () => {
+describe("Funcionalidade: Cadastro no Hub de Leitura", () => {
+	beforeEach(() => {
+		cy.visit("register.html");
+	});
 
-  beforeEach(() => {
-    cy.visit('register.html')
-  });
+	it("Deve fazer o cadastro com sucesso usando função JS", () => {
+		let nome = `Usuario Teste ${Date.now()}`;
+		let email = `teste${Date.now()}@teste.com`;
+		cy.get("#name").type(nome);
+		cy.get("#email").type(email);
+		cy.get("#phone").type("(51) 99999-9999");
+		cy.get("#password").type("123456*");
+		cy.get("#confirm-password").type("123456*");
+		cy.get("#terms-agreement").check();
+		cy.get("#register-btn").click();
+		cy.url().should("include", "dashboard");
+	});
 
-  it('Deve fazer o cadastro com sucesso usando função JS', () => {
-    let nome = `Usuario Teste ${Date.now()}`
-    let email = `teste${Date.now()}@teste.com`
-    cy.get('#name').type(nome)
-    cy.get('#email').type(email)
-    cy.get('#phone').type('(51) 99999-9999')
-    cy.get('#password').type('123456*')
-    cy.get('#confirm-password').type('123456*')
-    cy.get('#terms-agreement').check()
-    cy.get('#register-btn').click()
-    // Resultado esperado
-    cy.url().should('include', 'dashboard')
-  });
+	it("Deve fazer o cadastro com sucesso usando Faker", () => {
+		let nome = faker.person.fullName();
+		let email = faker.internet.email();
+		cy.get("#name").type(nome);
+		cy.get("#email").type(email);
+		cy.get("#phone").type("(51) 99999-9999");
+		cy.get("#password").type("123456*");
+		cy.get("#confirm-password").type("123456*");
+		cy.get("#terms-agreement").check();
+		cy.get("#register-btn").click();
+		cy.url().should("include", "dashboard");
+		cy.get("#user-name").should("contain", nome);
+	});
 
-  it('Deve fazer o cadastro com sucesso usando Faker', () => {
-    let nome = faker.person.fullName()
-    let email = faker.internet.email()
-    cy.get('#name').type(nome)
-    cy.get('#email').type(email)
-    cy.get('#phone').type('(51) 99999-9999')
-    cy.get('#password').type('123456*')
-    cy.get('#confirm-password').type('123456*')
-    cy.get('#terms-agreement').check()
-    cy.get('#register-btn').click()
-    // Resultado esperado
-    cy.url().should('include', 'dashboard')
-    cy.get('#user-name').should('contain', nome)
-  });
-
-  it.only('Deve fazer o cadastro com sucesso usando comando customizado', () => {
-    const email = `Teste${Date.now()}@teste.com`
-    const nome = faker.person.fullName()
-    cy.preencherCadastro(
-      nome,
-      email,
-      '(51)99999-9999',
-      '123456*',
-      '123456*'
-    )
-    cy.url().should('include', 'dashboard')
-  });
-
-
-})
+	it("Deve fazer o cadastro com sucesso usando comando customizado", () => {
+		const email = `Teste${Date.now()}@teste.com`;
+		const nome = faker.person.fullName();
+		cy.preencherCadastro(nome, email, "(51)99999-9999", "123456*", "123456*");
+		cy.url().should("include", "dashboard");
+	});
+});
