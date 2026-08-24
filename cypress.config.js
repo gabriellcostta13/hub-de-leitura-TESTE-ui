1d563bb30f66459d0984769f6f1869289197b152
Gabriel Costa <gabriellcostta13@gmail.com>
2026-08-24 17:56:19 -0300

    atualizações do cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
+	allowCypressEnv: false,
 
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-    baseUrl: 'http://localhost:3000'
-  },
+	e2e: {
+		setupNodeEvents(on, config) {
+			// implement node event listeners here
+		},
+		baseUrl: "http://localhost:3000",
+		projectId: "sj8vko",
+    video:true
+	},
 });
